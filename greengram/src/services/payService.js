8844a7da7b555dc750acb1e499d7693f97a989f2
d-kjh <dkjh9942@gmail.com>
2025-08-29 14:26:29 +0900

    pay

---
+++ greengram/src/services/payService.js
@@ -0,0 +1,8 @@
+import axios from './httpRequester';
+
+const path = '/pay';
+
+export const postPayReady = () => axios.post(`${path}/ready`);
+
+export const getPayApprove = (params) =>
+  axios.get(`${path}/approve`, { params });
